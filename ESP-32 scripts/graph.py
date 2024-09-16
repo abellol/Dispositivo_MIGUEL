831576f9d60b6f9a37f6d3a9e450cb7cdb227a35
fcastellanos1 <54986954+fcastellanos1@users.noreply.github.com>
2024-09-16 17:26:49 -0400

    Added functions
    Blank dataframe and update value

--- ESP-32 scripts/graph.py
+++ ESP-32 scripts/graph.py
@@ -11,14 +11,25 @@ def init_df(n_rows):
 
 
 def graph_df(df):
-    # Graficar en coordenadas polares
+    # Graficar el dataframe en coordenadas polares
     plt.figure(figsize=(6, 6))
     ax = plt.subplot(111, polar=True)
     ax.plot(df['theta'], df['R'])
     ax.set_title(r'Distancia($\theta$)')
     plt.show()
 
-    
+
+def update_value_df(df, theta, distance):
+    # Función para actualizar un único valor R dentro del dataframe
+    index = df[df['theta'] == theta].index
+    df.loc[index, 'R'] = distance
+
+
+def blank_df(df):
+    # Función para borrar todos los datos de la columna R dentro del dataframe
+    df['R'] = np.nan
+
+
 if __name__ == "__main__":
     # Crea un dataframe de pandas para guardar los datos y los grafica
     df = init_df(52)
