27257070bdff931043dbbb5f005cadbb1e138b07
fcastellanos1 <54986954+fcastellanos1@users.noreply.github.com>
2024-09-16 14:51:05 -0400

    Archivo para graficar en coordenadas polares
    Versión preliminar, todavía no lee datos de ninguna entrada. Pero es fácilmente configurable debido al dataframe de pandas

---
+++ ESP-32 scripts/graph.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def init_df(n_rows):
+    # Datos de ejemplo
+    theta = np.linspace(0, 2 * np.pi, n_rows)  # Ángulo theta
+    R = np.abs(np.sin(theta))  # Radio R, por ejemplo, función seno absoluto
+    df = pd.DataFrame({'theta': theta, 'R': R})
+    return df
+
+
+def graph_df(df):
+    # Graficar en coordenadas polares
+    plt.figure(figsize=(6, 6))
+    ax = plt.subplot(111, polar=True)
+    ax.plot(df['theta'], df['R'])
+    ax.set_title(r'Distancia($\theta$)')
+    plt.show()
+
+    
+if __name__ == "__main__":
+    # Crea un dataframe de pandas para guardar los datos y los grafica
+    df = init_df(52)
+    print(df)
+    graph_df(df)
